c47fb558c100e8182b14bd53d522f3c3aa61326f
OscarCreator <oscar.creator13@gmail.com>
2020-08-18 09:34:18 +0200

    Tutorial 20: Mipmapping

--- src/renderEngine/Loader.java
+++ src/renderEngine/Loader.java
@@ -2,10 +2,7 @@ package renderEngine;
 
 import models.RawModel;
 import org.lwjgl.BufferUtils;
-import org.lwjgl.opengl.GL11;
-import org.lwjgl.opengl.GL15;
-import org.lwjgl.opengl.GL20;
-import org.lwjgl.opengl.GL30;
+import org.lwjgl.opengl.*;
 import org.newdawn.slick.opengl.Texture;
 import org.newdawn.slick.opengl.TextureLoader;
 
@@ -50,6 +47,14 @@ public class Loader {
 		Texture texture = null;
 		try {
 			texture = TextureLoader.getTexture("PNG", new FileInputStream("res/" + fileName + ".png"));
+			//Generate all lower resolution images
+			GL30.glGenerateMipmap(GL11.GL_TEXTURE_2D);
+			//tell OpenGL to use render lower resolution when surface area is smaller to the camera.
+			//LINEAR_MIPMAP_LINEAR = transition smoothly between different resolutions.
+			//LINEAR_MIPMAP_NEAREST = transition is less smooth between different resolutions.
+			GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR_MIPMAP_LINEAR);
+			//LOD = level of detail, more negative means higher resolutions further away than usual
+			GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL14.GL_TEXTURE_LOD_BIAS, -0.4f);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
