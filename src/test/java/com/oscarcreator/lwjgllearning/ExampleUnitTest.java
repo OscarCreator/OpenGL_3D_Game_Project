94ecca4741081f00bc639f3af586c65092c7b898
OscarCreator <oscar.creator13@gmail.com>
2020-08-20 16:29:28 +0200

    Added Junit jars and a simple test class.

---
+++ src/test/java/com/oscarcreator/lwjgllearning/ExampleUnitTest.java
@@ -0,0 +1,18 @@
+package com.oscarcreator.lwjgllearning;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Oscar on 2020-08-19.
+ */
+
+public class ExampleUnitTest {
+
+	@Test
+	public void test_OnePlusOne_IsTwo(){
+		assertEquals(4, 2 + 2);
+	}
+
+}
